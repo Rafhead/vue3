0a8812b050b35e340ca8ce806c17e64283eedad3
Khaliakbarov, Rafkhad <rafhadh@gmail.com>
2021-12-07 23:37:09 +0100

    Added simple header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <ul v-show="show" class="list-group" style="margin-left: 5px">
+    <li :key="task" v-for="task in tasks" class="list-group-item" :class="task.priority" style="width: 500px">
+      {{ task.name }}
+    </li>
+  </ul><br>
+
+  <button v-if="!show" @click="changeShow" class="btn btn-secondary" style="margin-left: 5px">Show tasks</button>
+  <button v-else @click="changeShow" class="btn btn-secondary" style="margin-left: 5px">Hide Tasks</button><br><br>
+
+  <label for="task" style="margin-left: 5px">New task: </label>
+  <input type="text" id="newTask" name="task" v-model="newTask" style="margin-left: 10px;"/><br><br>
+
+  <div v-show="invalidNewTask" class="alert alert-warning" role="alert" style="width: 500px">Please enter a non empty value!</div>
+  <button @click="addNewTask()" class="btn btn-success" style="margin-left: 5px">Add Task</button>
+
+  
+
+  
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+  },
+  data() {
+    return {
+      tasks: [
+                {name: "Learn React", priority: "Low"},
+                {name: "Learn Vue", priority: "Medium"},
+                {name: "Become a Web-Programmer", priority: "High"},
+                ],
+            show: true,
+            newTask: "",
+            invalidNewTask: false,
+    } 
+  },
+  methods: {
+        changeShow(){
+            this.show = !this.show;
+        },
+        addNewTask(){
+            if(this.newTask === ""){
+                this.invalidNewTask = true;
+                return;
+            }
+            newTask = {name: this.newTask, priority: "Medium"}
+            this.tasks.push(newTask);
+            this.newTask = "";
+            this.invalidNewTask = false;
+         },
+    },
+    computed: {
+    }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.High {
+    background-color: lightcoral;
+}
+
+.Medium {
+    background-color: lightsalmon;
+}
+
+.Low {
+    background-color: bisque;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+		<div class="tabs">
+      <a href="#">About</a>
+      <a href="#">Jobs</a>
+      <a href="#">Infrastructure</a>
+    </div>
+  </header>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style>
+header {
+	background-color: wheat;
+	position: sticky;
+  height: 55px;
+  margin-bottom: 10px;
+}
+
+.tabs {
+  float: right;
+}
+
+a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 10px;
+  font-size: 18px; 
+  border-radius: 4px;
+  text-decoration: none;
+  margin-top: 2px;
+}
+
+a:hover {
+  background-color: gray;
+  color: black;
+}
+</style>
